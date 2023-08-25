8b4b8e102c946bc552d291cc30ef0cadee5e7bd4
Dmitry Kosintsev <dmitry.kosintsev@gmail.com>
2023-08-25 21:39:08 +0800

    Initial Commit

---
+++ ConverIt/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ConverIt
+//
+//  Created by MacAir on 2023.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ConverIt/ConverItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverItApp.swift
+//  ConverIt
+//
+//  Created by MacAir on 2023.08.25.
+//
+
+import SwiftUI
+
+@main
+struct ConverItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
